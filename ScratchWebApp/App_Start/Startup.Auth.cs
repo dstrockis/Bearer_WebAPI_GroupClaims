8a2ecd08e896bbb42e80163df75027387c32488e
dstrockis <dastrock@microsoft.com>
2014-11-14 15:00:43 -0800

    Initial Commit

---
+++ ScratchWebApp/App_Start/Startup.Auth.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Owin.Security.ActiveDirectory;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OAuth;
+using Microsoft.Owin.Security;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScratchWebApp
+{
+    public partial class Startup
+    {
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            //app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+
+            //app.UseCookieAuthentication(new CookieAuthenticationOptions());
+
+        }
+    }
+}
---
+++ ScratchWebApp/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(ScratchWebApp.Startup))]
+
+namespace ScratchWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
