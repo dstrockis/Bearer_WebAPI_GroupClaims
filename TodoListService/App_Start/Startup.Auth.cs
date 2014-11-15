0faa90b9aa5978b9eb85ec6e6ac5e00dea12fb36
dstrockis <dastrock@microsoft.com>
2014-11-14 18:23:27 -0800

    right repo

---
+++ TodoListService/App_Start/Startup.Auth.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.ActiveDirectory;
+using Owin;
+using System.IdentityModel.Tokens;
+using TodoListService.Utils;
+
+namespace TodoListService
+{
+    public partial class Startup
+    {
+        // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.UseWindowsAzureActiveDirectoryBearerAuthentication(
+                new WindowsAzureActiveDirectoryBearerAuthenticationOptions
+                {
+                    Audience = ConfigurationManager.AppSettings["ida:Audience"],
+                    Tenant = ConfigurationManager.AppSettings["ida:Tenant"],
+                    TokenValidationParameters = new TokenValidationParameters { 
+                        RoleClaimType = "roles",
+                    },
+                    TokenHandler = new MyJwtSecurityTokenHandler(),
+                    
+                });
+        }
+    }
+}
---
+++ TodoListService/Utils/MyJwtSecurityTokenHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Web.Helpers;
+
+namespace TodoListService.Utils
+{
+    class MyJwtSecurityTokenHandler : JwtSecurityTokenHandler
+    {
+        public override ClaimsPrincipal ValidateToken(string securityToken, TokenValidationParameters validationParameters, out SecurityToken validatedToken)
+        {
+            SecurityToken st = null; 
+            ClaimsPrincipal cp = base.ValidateToken(securityToken, validationParameters, out st);
+            if (st != null)
+            {
+                validatedToken = st;
+                ClaimsIdentity ci = cp.Identity as ClaimsIdentity;
+
+                if (ci.FindFirst("_claim_names") != null && (Json.Decode(claimsId.FindFirst("_claim_names").Value)).groups != null)
+                {
+                    // Lookup the groups from the graph, add group claims to ci.
+                }
+
+                foreach (Claim groupclaim in ci.FindAll("groups"))
+                {
+                   // Translate the group claims into role claims
+                }
+                return new ClaimsPrincipal(ci);
+            }
+            else 
+            {
+                throw new SecurityTokenValidationException("something weird happened");
+            }
+        }
+    }
+}
